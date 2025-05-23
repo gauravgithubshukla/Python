d47c4d42b12aab20851cc372c7ebe580b38b2305
Gaurav Kumar Shukla <67834228+gauravgithubshukla@users.noreply.github.com>
2025-05-23 08:29:16 +0530

    Add files via upload

---
+++ Assignment2/Assign2_Task2_SumofInt.py
@@ -0,0 +1,7 @@
+#Display the sum of integers.
+limit=int(input("Enter the limit:"))
+sum=0
+for i in range(1,limit+1):
+    sum=sum+i
+print("The sum of numbers from 1 to ",limit," is :",sum)
+print("Thankyou!")
