8d5e2811741922e9bdee6b94947d07a338aeeded
Gaurav Kumar Shukla <67834228+gauravgithubshukla@users.noreply.github.com>
2025-05-23 08:22:51 +0530

    Add files via upload

---
+++ Assignment1/Assign1_Task1_Basic_Math_Operation.py
@@ -0,0 +1,20 @@
+#Basic Mathematical operation
+
+#input two numbers
+num1=input("Enter first number: ")
+num2=input("Enter second number: ")
+num1=int(num1)
+num2=int(num2)
+
+#basic operation
+addittion = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+
+#display output
+print("Addittion: ",addittion)
+print("Subtraction: ",subtraction)
+print("Multiplication: ",multiplication)
+print("Division: ",division)
---
+++ Assignment1/Assign1_Task2_Personal_Greeting.py
@@ -0,0 +1,7 @@
+#Personalized Greeting Display
+fName=input("Enter your first name: ")
+lName=input("Enter your last name: ")
+name=fName + lName
+
+#Display output
+print(" Hello, "+name+"! Welcome to  the Python Program.") 
